6c5029a91d8404ef20d705da988eb457c30ac725
ZingLix <me@zinglix.xyz>
2019-02-26 22:07:40 +0800

    update usage

--- psyche-http/main.cpp
+++ psyche-http/main.cpp
@@ -1,12 +1,26 @@
 #include "HttpServer.h"
 #include <fcntl.h>
+#include <iostream>
+#include <dirent.h>
 
 int main(int argc, char* argv[]) {
+    if(argc==1) {
+        std::cout << "Please input the website root directory.";
+        return 0;
+    }
+    DIR* d = opendir(argv[1]);
+    if(d==nullptr) {
+        std::cout << "Directory doesn't exist.";
+        return -1;
+    }
+    closedir(d);
     HttpServer server(8898);
-    server.setRecvCallback([](HttpMessage & recvmsg, HttpMessage & sendmsg) {
+    std::string rootpath(argv[1]);
+    if (rootpath.back() == '/') rootpath.pop_back();
+    server.setRecvCallback([&](HttpMessage & recvmsg, HttpMessage & sendmsg) {
         std::string path = recvmsg.getPath();
         if (path.back() == '/') path += "index.html";
-        path = "/home/vs/blog" + path;
+        path = rootpath + path;
         int fd = open(path.c_str(), O_RDONLY);
         if(fd==-1) {
             sendmsg.setStatusCode(404);
