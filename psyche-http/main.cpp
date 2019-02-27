b4d80fbc2569bc4d594d71b12084cd1525d82f93
ZingLix <me@zinglix.xyz>
2019-02-27 19:44:36 +0800

    refactor

--- psyche-http/main.cpp
+++ psyche-http/main.cpp
@@ -8,15 +8,14 @@ int main(int argc, char* argv[]) {
         std::cout << "Please input the website root directory.";
         //return 0;
     }
-  //  DIR* d = opendir(argv[1]);
-   // if(d==nullptr) {
-   //     std::cout << "Directory doesn't exist.";
-   //     return -1;
- //   }
- //   closedir(d);
+    DIR* d = opendir(argv[1]);
+    if(d==nullptr) {
+        std::cout << "Directory doesn't exist.";
+        return -1;
+    }
+    closedir(d);
     HttpServer server(8898);
-//    std::string rootpath(argv[1]);
-    std::string rootpath("/home/vs/blog");
+    std::string rootpath(argv[1]);
     if (rootpath.back() == '/') rootpath.pop_back();
     server.setRecvCallback([&](HttpRequest & recvmsg, HttpResponse & sendmsg) {
         std::string path = recvmsg.getPath();
